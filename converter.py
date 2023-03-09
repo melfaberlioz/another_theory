3f9df83adfe37f89faa38bfe7a868cdb5e3c5199
melfaberlioz <sofi.horizon@gmail.com>
2023-03-09 11:48:23 +0200

    Initial Commit

---
+++ converter.py
@@ -0,0 +1,17 @@
+dollar = 40.25
+number_of_el = int(input("Enter the number of value you want to convert: "))
+
+# for value in range(n):
+#     money = int(input("Enter the value you want to convert to USD:"))
+#     print(f"Converted values: {money * dollar} $")
+
+for element in range(number_of_el):
+    money = input("Enter the value you want to convert: ")
+    parts = money.split()
+    currency = parts[1]
+    value = int(parts[0])
+    if currency == 'грн':
+        print(f"{round(value / dollar, 2)} $")
+        # print(f"{value / dollar:6.2f} $")
+    else:
+        print(f"{value * dollar} грн")
---
+++ for_loop.py
@@ -0,0 +1,3 @@
+friends = ['Joseph', 'Glenn', 'Sally'] #a list, a SET of sth
+for friend in friends:
+    print('Happy New Year:', friend)
